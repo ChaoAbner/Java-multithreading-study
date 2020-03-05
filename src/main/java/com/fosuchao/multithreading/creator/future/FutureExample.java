ae11fddeec289ac38ad54a80856a826577356016
ChaoAbner <2473109110@qq.com>
2020-03-05 19:52:00 +0800

    update future example

--- src/main/java/com/fosuchao/multithreading/creator/future/FutureExample.java
+++ src/main/java/com/fosuchao/multithreading/creator/future/FutureExample.java
@@ -8,13 +8,21 @@ import java.util.concurrent.FutureTask;
  * @Auther: Joker Ye
  * @Date: 2020/2/4 09:43
  */
-public class UseFuture {
+public class FutureExample {
     public static void main(String[] args) throws Exception{
         Callable demo = new Demo();
         FutureTask<Integer> integerFutureTask = new FutureTask<Integer>(demo);
         new Thread(integerFutureTask).start();
-        System.out.println("干别的事情");
+        doOtherTask();
+        long start = System.currentTimeMillis();
         System.out.println("线程执行完成，返回结果为：" + integerFutureTask.get());
+        long end = System.currentTimeMillis();
+        System.out.println("取回结果的时间为：" + (end - start) + "ms");
+    }
+
+    public static void doOtherTask() throws InterruptedException {
+        System.out.println("正在做干别的事情");
+        Thread.sleep(1000);
     }
 }
 
@@ -25,7 +33,7 @@ class Demo implements Callable<Integer> {
     public Integer call() throws Exception {
         // 线程中执行的逻辑
         System.out.println("计算结果中。。。");
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         return 1;
     }
 }
